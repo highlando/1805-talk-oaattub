c879e5cf92395ef659162fa623cbbbeaac84965b
highlando <jnhlnd@gmail.com>
2016-02-16 08:31:22 +0100

    added all

---
+++ slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Paywalls, Preprints, Science Platforms',
+    subtitle: 'Open Access Day @ MPI-Magdeburg, Feb 16, 2016',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/matheon-logo-color.svg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['mpcsc'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+      {
+        name: 'Robert Flassig',
+        company: 'MPI Magdeburg, psf group',
+      },
+      {
+        name: 'Jan Heiland',
+        company: 'MPI Magdeburg, csc group',
+        twitter: 'highlando',
+        github: 'http://github.com/highlando'
+      }
+  ]
+};
+
